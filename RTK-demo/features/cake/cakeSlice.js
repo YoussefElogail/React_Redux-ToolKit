22e592a3429fea454579f4bc31780203f3478c85
YoussefElogail <125933702+YoussefElogail@users.noreply.github.com>
2023-10-24 21:07:44 +0300

    Cake Slice Feature

---
+++ RTK-demo/features/cake/cakeSlice.js
@@ -0,0 +1,20 @@
+const createSlice = require("@reduxjs/toolkit").createSlice
+const initialState = {
+  numberOfCakes : 10
+}
+const cakeSlice = createSlice({
+  name : "cake",
+  initialState  ,
+  reducers : {
+    ordered : (state , action) => {
+      state.numberOfCakes --
+    },
+    reStocked : (state , action) => {
+      state.numberOfCakes += action.payload
+    }
+  }
+})
+
+module.exports = cakeSlice.reducer
+
+module.exports.cakeActions = cakeSlice.actions
