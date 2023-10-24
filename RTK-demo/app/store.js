b7c3af019c0abade923ccb6a2edadb86c449d99c
YoussefElogail <125933702+YoussefElogail@users.noreply.github.com>
2023-10-24 22:24:15 +0300

    Logger Middleware.

--- RTK-demo/app/store.js
+++ RTK-demo/app/store.js
@@ -1,13 +1,18 @@
 const configureStore = require("@reduxjs/toolkit").configureStore
 
+const { getDefaultMiddleware } = require("@reduxjs/toolkit")
 const cakeReducer = require("../features/cake/cakeSlice")
 const icecreamReducer = require("../features/iceCream/iceCreamSlice")
+
+const reduxLogger = require("redux-logger")
+const logger = reduxLogger.createLogger()
 
 const store = configureStore({
   reducer : {
     cake : cakeReducer,
     icecream : icecreamReducer
-  }
+  },
+  middleware: (getDefaultMiddleware)=> getDefaultMiddleware().concat(logger)
 })
 
 module.exports = store
