f1a37a90f833a68a87c2a79dd537428ea2f26c05
YoussefElogail <125933702+YoussefElogail@users.noreply.github.com>
2023-10-25 20:52:55 +0300

    Fetching Data

--- react-rtk-demo/features/user/usersList.jsx
+++ react-rtk-demo/features/user/usersList.jsx
@@ -1,10 +1,22 @@
-
-const usersList = () => {
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "./userSlice";
+const UsersList = () => {
+  const users = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
   return (
     <>
-    <h2>List of users:</h2>
+      <h2>List of users:</h2>
+      {users.loading ? "loading...." : null}
+      {users.error ? (<h4>{users.error}</h4>):null}
+      {users.data && (<ul>
+        {users.data.map((user)=> (<li key={user.id}>{user.name}</li>))}
+      </ul>)}
     </>
-  )
-}
+  );
+};
 
-export default usersList
+export default UsersList;
