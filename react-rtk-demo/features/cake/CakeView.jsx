e5b847d1d15e54737fa2e163256563edc365fac7
YoussefElogail <125933702+YoussefElogail@users.noreply.github.com>
2023-10-25 19:57:33 +0300

    useDispatch

--- react-rtk-demo/features/cake/CakeView.jsx
+++ react-rtk-demo/features/cake/CakeView.jsx
@@ -1,13 +1,14 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { cakeActions } from "./cakeSlice";
 
 const CakeView = () => {
   const cake = useSelector((state) => state.cake.numberOfCakes);
-
+  const dispatch = useDispatch()
   return (
     <>
       <h1>Number of cakes: {cake}</h1>
-      <button>Order cake</button>
-      <button>Restock cake</button>
+      <button onClick={()=> dispatch(cakeActions.ordered())}>Order cake</button>
+      <button onClick={()=> dispatch(cakeActions.reStocked(10))}>Restock cake</button>
     </>
   );
 };
--- react-rtk-demo/features/iceCream/IceCreamView.jsx
+++ react-rtk-demo/features/iceCream/IceCreamView.jsx
@@ -1,13 +1,14 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { iceCreameActions } from "./iceCreamSlice";
 
 const IceCreamView = () => {
   const iceCream = useSelector((state) => state.icecream.numberOfIceCreams);
-
+  const dispatch = useDispatch()
   return (
     <>
       <h1>Number of ice-cream: {iceCream}</h1>
-      <button>Order ice-cream</button>
-      <button>Restock ice-cream</button>
+      <button onClick={()=> dispatch(iceCreameActions.ordered())}>Order ice-cream</button>
+      <button onClick={()=> dispatch(iceCreameActions.reStocked(10))}>Restock ice-cream</button>
     </>
   );
 };
